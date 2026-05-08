9a9f4f65b2b5a1894f0cf5aba79142dfb545eb3e
isms01 <ishimasa01@gmail.com>
2026-05-08 19:47:44 +0900

    Add: three-integer-sum - submission-19

---
+++ Data Structures & Algorithms/three-integer-sum/submission-19.py
@@ -0,0 +1,12 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        return_list = []
+        for i, _ in enumerate(nums):
+            for j in range(i+1,len(nums)-1):
+                target = -1 * (nums[i] + nums[j])
+                if target in nums[j+1:len(nums)]:
+                    candidate = sorted([nums[i],nums[j],target])
+                    if candidate not in return_list:
+                        return_list.append(candidate)
+                        continue
+        return return_list
